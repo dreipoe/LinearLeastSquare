e526baabdf0366c848fe2fa5486976bf94c3938f
Alexander Oreshkin <oreshkin1996@mail.ru>
2019-03-28 15:08:00 +0500

    Update LeastSquare.java

--- LeastSquare.java
+++ LeastSquare.java
@@ -6,16 +6,15 @@ public class LeastSquare {
     
     public LeastSquare(double[] y) {
         int n = y.length;
+        double k = (2 * n + 1) / 3;
         double sumX = n * (n + 1) / 2;
-        double sumX2 = sumX * (2 * n + 1) / 3;
+        double sumX2 = sumX * k;
         double sumY = 0, sumXY = 0;
         
         for (int x = 0; x < n; x++) {
             sumY += y[x];
             sumXY += (x + 1) * y[x];
         }
-        
-        double k = sumX2 / sumX;
         
         b = (sumXY - sumY * k) / (sumX - n * k);
         a = (sumXY - sumX * b) / sumX2;
